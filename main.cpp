a7c92b3ea5d45ac93664afb33ec3a70bb7128b6b
Warlock2909 <152647017+Warlock2909@users.noreply.github.com>
2024-05-29 13:55:46 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	//comment
+	/*comment
+	* comment
+	*/
+	setlocale(LC_ALL, "RU");
+	std::cout << "Привет Мир\n";
+	std::cout << "All is okay\n" <<std::endl;
+	std::cout << "Hello";
+	return 0;
+}
